2f5d282d5bc9bd499e26fb4399516d6e6ed5c5f6
Bradley Grose <47512745+bgrose@users.noreply.github.com>
2019-11-20 16:48:36 -0500

    Add files via upload

---
+++ GradeCalculator/src/SectionType.java
@@ -0,0 +1,12 @@
+/*
+ * BRadley Grose
+ */
+public class SectionType {
+	public static final String LABS = "LABS";
+	public static final String LAB_REPORTS = "LABREPORTS";
+	public static final String HOMEWORK = "HOMEWORK";
+	public static final String EXAM01 = "EXAM 1";
+	public static final String EXAM02 = "EXAM 2";
+	public static final String FINAL = "FINAL";
+	
+}
